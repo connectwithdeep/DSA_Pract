77773a299704ae8221caa1a8b5ad8f484d79de69
connectwithdeep <138895597+connectwithdeep@users.noreply.github.com>
2024-01-16 01:51:07 +0530

    did some more recursion problems

--- DSA/recursion_probs.cpp
+++ DSA/recursion_probs.cpp
@@ -15,6 +15,18 @@ bool isPalindrome(string& str) {
 
     return isPalindrome(str);
 }
+
+//sum of n-terms
+long long sum =0;
+long long sumFirstN(long long n) {
+    if(n==0){
+        return 0;
+    }
+    sum+=n;
+    sumFirstN(n-1);
+    return sum;
+}
+
 //reversing an array
 vector<int> reverseArray(int n, vector<int> &nums)
 {
@@ -24,6 +36,25 @@ vector<int> reverseArray(int n, vector<int> &nums)
     }
 return result;
 }
+
+// reversing array using recursion 
+int temp;
+void reverseArrayHelper(vector<int> &nums, int start, int end) {
+    if (start >= end) {
+        return;
+    }
+
+    // swap(nums[start], nums[end]);
+    temp=nums[start];
+    nums[start]=nums[end];
+    nums[end]=temp;
+    reverseArrayHelper(nums, start + 1, end - 1);
+}
+vector<int> reverseArray(int n, vector<int> &nums) {
+    reverseArrayHelper(nums, 0, n - 1);
+    return nums;
+}
+
 //factorial
 vector<long long> factorialNumbers(long long n) {
     vector<long long> result;
@@ -38,4 +69,14 @@ vector<long long> factorialNumbers(long long n) {
 
 int main(){
 
+}
+
+//fibonacci number
+int fib(int n){
+    if(n<=1){
+        return n;
+    }
+    int first=fib(n-1);
+    int second=fib(n-2);
+    return first+second;
 }
