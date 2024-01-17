ab61e8089613f592d0774edb9ae96b84fe5e0002
connectwithdeep <138895597+connectwithdeep@users.noreply.github.com>
2024-01-18 01:21:11 +0530

    learnt hashing

--- DSA/hashing.cpp
+++ DSA/hashing.cpp
@@ -4,15 +4,20 @@ using namespace std;
 
 int main()
 {   int n;
-    int count=0;
-    int arr[8]={1,3,5,7,3,1,2,4};
     cout<<"Enter the number : ";
     cin>>n;
-    for(int i=0;i<=7;i++){
-        if(arr[i]==n){
-            count++;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
         }
-    }
-    cout<<"\n The number was "<<count<<" times in the array";
+    int hash[13]={0};// can be upto 1000000 in main() and 10000000 in global scope for int and *10 extra for bool;
+    for(int i=0;i<n;i++){
+            hash[arr[i]]+=1;
+        }
+    cout<<"Enter the number you want to find the occurence of ";
+        int number;
+        cin>>number;
+        cout<<hash[number]<<" times"<<endl;
+    
     return 0;
 }
