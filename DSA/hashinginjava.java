d409b955ccf7edb9ae0cc79740e90f34ca7c428b
connectwithdeep <138895597+connectwithdeep@users.noreply.github.com>
2024-01-25 00:18:01 +0530

    Update hashinginjava.java

--- DSA/hashinginjava.java
+++ DSA/hashinginjava.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Main
-{
+{	
 	public static void main(String[] args) {
 		int[] arr={1,2,3,1,1,2,3,4,5,8};
 		int[] hash= new int[13];
@@ -12,5 +12,18 @@ public class Main
 		int num;
 		num= sc.nextInt();
         System.out.print(hash[num]);
+	}
+	//for character hashing
+	public static void CharacterHash() {
+	char[] arr={'a','b','c','d','e','a','a','e','v','e'};
+	int[] hash= new int[26];
+	for(int i=0;i<10;i++){
+		hash[arr[i]-'a']+=1;
+		}
+	System.out.print("Enter the number to get its frequency ");
+        Scanner sc=new Scanner(System.in);
+		char num;
+		num= sc.next().charAt(0);
+        System.out.print(hash[num-'a']);
 	}
 }
